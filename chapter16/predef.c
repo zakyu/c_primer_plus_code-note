c72713386f3ec23d3eb86071ad21ec1d7f647360
zakyu <zekaiyu.97@gmail.com>
2022-12-11 16:48:40 +0800

    update

---
+++ chapter16/predef.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void why_me();
+
+int main(void)
+{
+    printf("The file is %s.\n", __FILE__);
+    printf("The date is %s.\n", __DATE__);
+    printf("The time is %s.\n", __TIME__);
+    printf("The version is %ld.\n", __STDC_VERSION__);
+    printf("This is line %d.\n", __LINE__);
+    printf("This function is %s.\n", __func__);
+    why_me();
+
+    exit(1);
+    return 0;
+}
+
+void why_me()
+{
+    printf("This function is %s\n", __func__);
+    printf("This is line %d.\n", __LINE__);
+}
